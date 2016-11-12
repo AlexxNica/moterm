d48b4bfdc6e394eebb67e166eca9dea8dd506e96
Jérôme Poichet <jpoichet@google.com>
2016-11-11 17:51:26 -0800

    [moterm] Generate proper VT100 key sequence
    For Ctrl+[a..z] VT100 expects special characters.
    
    Change-Id: I60a643a21933b5d82f36ecded073d4310179b0c0

--- key_util.cc
+++ key_util.cc
@@ -23,7 +23,10 @@ std::string GetInputSequenceForKeyPressedEvent(const mozart::Event& key_event,
   //               << "\n  hid_usage = " << key_data.hid_usage
   //               << "\n  code_point = " << key_data.code_point;
 
-  if (key_data.code_point) {
+  uint32_t non_ascii =
+      key_data.modifiers & (mozart::CONTROL | mozart::ALT | mozart::GUI);
+
+  if (!non_ascii && key_data.code_point) {
     if (key_data.code_point > 128) {
       FTL_NOTIMPLEMENTED();
       return std::string();
@@ -61,6 +64,62 @@ std::string GetInputSequenceForKeyPressedEvent(const mozart::Event& key_event,
       return std::string("\n");
     case HID_USAGE_KEY_TAB:
       return std::string("\t");
+    case HID_USAGE_KEY_A:
+      if (key_data.modifiers & mozart::CONTROL) {
+        return std::string(1, 1);
+      }
+    case HID_USAGE_KEY_B:
+      if (key_data.modifiers & mozart::CONTROL) {
+        return std::string(1, 2);
+      }
+    case HID_USAGE_KEY_C:
+      if (key_data.modifiers & mozart::CONTROL) {
+        return std::string(1, 3);
+      }
+    case HID_USAGE_KEY_D:
+      if (key_data.modifiers & mozart::CONTROL) {
+        return std::string(1, 4);
+      }
+    case HID_USAGE_KEY_E:
+      if (key_data.modifiers & mozart::CONTROL) {
+        return std::string(1, 5);
+      }
+    case HID_USAGE_KEY_F:
+      if (key_data.modifiers & mozart::CONTROL) {
+        return std::string(1, 6);
+      }
+    case HID_USAGE_KEY_H:
+      if (key_data.modifiers & mozart::CONTROL) {
+        return std::string(1, 8);
+      }
+    case HID_USAGE_KEY_K:
+      if (key_data.modifiers & mozart::CONTROL) {
+        return std::string(1, 11);
+      }
+    case HID_USAGE_KEY_L:
+      if (key_data.modifiers & mozart::CONTROL) {
+        return std::string(1, 12);
+      }
+    case HID_USAGE_KEY_N:
+      if (key_data.modifiers & mozart::CONTROL) {
+        return std::string(1, 14);
+      }
+    case HID_USAGE_KEY_P:
+      if (key_data.modifiers & mozart::CONTROL) {
+        return std::string(1, 16);
+      }
+    case HID_USAGE_KEY_T:
+      if (key_data.modifiers & mozart::CONTROL) {
+        return std::string(1, 20);
+      }
+    case HID_USAGE_KEY_U:
+      if (key_data.modifiers & mozart::CONTROL) {
+        return std::string(1, 21);
+      }
+    case HID_USAGE_KEY_W:
+      if (key_data.modifiers & mozart::CONTROL) {
+        return std::string(1, 23);
+      }
     default:
       FTL_NOTIMPLEMENTED() << " hid_usage = " << key_data.hid_usage;
   }
